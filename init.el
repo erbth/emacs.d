3a893f79f49dc273c8c91bae7d66366a58db6721
erbth <t.erbesdobler@gmx.de>
2025-05-25 19:52:13 +0200

    customize

--- init.el
+++ init.el
@@ -196,17 +196,5 @@
 ))
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(cmake-mode corfu markdown-mode org-roam protobuf-mode treemacs-evil
-				treemacs-projectile vertico yaml-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Prevent customnize from cluttering init.el
+(setq custom-file "~/.emacs.d/custom.el")
